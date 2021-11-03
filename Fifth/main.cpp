97f43285d50c6af5a2d9801d2ddcd4081462eb2b
litolax <ura_rat@mail.ru>
2021-11-03 11:40:58 +0300

    Pervoe

--- Fifth/main.cpp
+++ Fifth/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+void First()
+{
+    double y(0), x(0), a, b, h, pi = 3.1415, min, max, temp;
+    cout << "Введите a: " << endl;
+    cin >> a;
+    cout << "Введите b: " << endl;
+    cin >> b;
+    cout << "Введите h: " << endl;
+    cin >> h;
+    int index = 0;
+    min = max = a;
+    x = a;
+    for (x; x <= b;)
+    {
+        temp = y;
+        y = (x*cos(pi / 4) - pow(x, 2))/(1-2*x*cos(pi/4) + pow(x,2));
+        index++;
+        cout << "[" << index << "] При x = " << x << " y = " << y;
+        if (y > temp) cout << " Функция убывает" << endl;
+        else cout << " Функция возрастает" << endl;
+        x += h;
+        if (min > y) min = y;
+        if (max < y) max = y;
+    }
+    cout << "Min = " << min << " Max = " << max << endl;
+}
+
 int main() {
-    cout << "Тест";
+    First();
     return 0;
 }
