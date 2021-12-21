ea4b61b2b14fcd634d3caf4a75d277382fa97759
litolax <ura_rat@mail.ru>
2021-12-21 12:19:55 +0300

    Закочил девятую лабу

--- ninth/main.cpp
+++ ninth/main.cpp
@@ -7,20 +7,50 @@ int first(int n) {
     else return first(n / 10) + 1;
 }
 
-//float second(float a) {
-//    if (a < 1) return 0;
-//    else return sqrt(sqrt(a) + second(sqrt(a-1)));
-//}
+int firstWithout(int num) {
+    int count = 0;
+    while (num > 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
 
-//double second(int max, int n = 1) {
-//    if (n == max) return sqrt(n);
-//    return (sqrt(n + second(max, n+1)));
-//}
+double second(int max, int n = 1) {
+    if (n == max) return sqrt(n);
+    return (sqrt(n + second(max, n+1)));
+}
 
-int third(int n) {
+double secondWithout(int num) {
+    double y = 0;
+    for (int i = num; i > 0; i--) {
+        y = sqrt(i + y);
+    }
+    return y;
+}
+
+int factor(int n) {
     if(n==0) return 1;
     if(n==1) return 1;
-    return n*third(n-1);
+    return n*factor(n-1);
+}
+
+
+float third(float n) {
+    if (n == 0) return 0;
+    return 1.0/factor(int(n)) + third(n-1);
+}
+
+float thirdWithout(int num) {
+    double res = 0;
+    for (int i = 1; i <= num; i++) {
+        double n = 1;
+        for (int j = 1; j <= i; j++) {
+            n *= j;
+        }
+        res += 1/n;
+    }
+    return res;
 }
 
 int main() {
@@ -30,17 +60,20 @@ int main() {
     int n;
     cout << "Введите число: " << endl;
     cin >> n;
-    cout << first(n) << endl;
+    cout << "С рекурсией: " << first(n) << endl;
+    cout << "Без рекурсии: " << firstWithout(n) << endl;
 
     cout << "Задание №2" << endl;
     cout << "Введите n: " << endl;
     float a;
     cin >> a;
-//    cout << second(4) << endl;
+    cout << "С рекурсией: " << second(a) << endl;
+    cout << "Без рекурсии: " << secondWithout(a) << endl;
 
     cout << "Задание №3" << endl;
     cout << "Введите n: " << endl;
-    cin >> n;
-    cout << third(n);
+    cin >> a;
+    cout << "С рекурсией: " << third(a) << endl;
+    cout << "Без рукурсии: " << thirdWithout(a) << endl;
     return 0;
 }
